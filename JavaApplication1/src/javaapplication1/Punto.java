ba9bcdae220dcbec05781f5e14a56117e4a5489a
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2022-11-18 10:13:37 -0500

    Issues 1 & 3
    Make point inmmutable and separate the clses

---
+++ JavaApplication1/src/javaapplication1/Punto.java
@@ -0,0 +1,19 @@
+package javaapplication1;
+
+public final class Punto {
+
+    private int x;
+    private int y;
+
+    Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getx() {
+		return x;
+	}
+    public int gety() {
+		return y;
+	}
+}
